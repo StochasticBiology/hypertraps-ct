545fbb45cd1cb2419a3369b70523299b687584fc
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2021-01-24 22:46:16 +0100

    Add files via upload

--- infer-synthetic.sh
+++ infer-synthetic.sh
@@ -1,8 +1,13 @@
+# generate synthetic datasets
 cd Tests
 gcc -o3 generate.c -lm -o generate.ce
-./generate.ce 78
+./generate.ce 78 > generated.tmp
+
+# copy these to inference folder, and follow them
 cp synth* ../Inference
 cd ../Inference
+
+# compile and run hypertraps code
 gcc -o3 hypertraps-all.c -lm -o hypertraps-all.ce
 ./hypertraps-all.ce synth-0-data.txt synth-0-times.txt synth-0-times.txt 1 3 3 0 0 > tmp-synth-1 &
 ./hypertraps-all.ce synth-0-data.txt synth-0-times.txt synth-0-times.txt 2 3 3 0 0 > tmp-synth-2 &
