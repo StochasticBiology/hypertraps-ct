1c17ba2783a9dcdb067d67f61ec0f371775f441d
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2021-01-25 17:43:20 +0000

    Add files via upload

--- infer-verify.sh
+++ infer-verify.sh
@@ -13,8 +13,8 @@ cd ../Inference
 
 # compile and run hypertraps code
 gcc -o3 hypertraps-all.c -lm -o hypertraps-all.ce
-./hypertraps-all.ce ../VerifyData/synth-cross-samples-0.txt ../VerifyData/synth-cross-times-0.txt ../VerifyData/synth-cross-times-0.txt 1 2 5 1 0 > ../VerifyData/cross03125.tmp &
-./hypertraps-all.ce ../VerifyData/synth-cross-samples-1.txt ../VerifyData/synth-cross-times-1.txt ../VerifyData/synth-cross-times-1.txt 1 2 5 1 0 > ../VerifyData/cross13125.tmp &
-./hypertraps-all.ce ../VerifyData/synth-cross-samples-2.txt ../VerifyData/synth-cross-times-2.txt ../VerifyData/synth-cross-times-2.txt 1 2 5 1 0 > ../VerifyData/cross23125.tmp &
-./hypertraps-all.ce ../VerifyData/synth-hardcube-data.txt ../VerifyData/synth-hardcube-time.txt ../VerifyData/synth-hardcube-time.txt 1 2 5 1 0 > ../VerifyData/hardcube3125.tmp &
-./hypertraps-all.ce ../VerifyData/synth-easycube-data.txt ../VerifyData/synth-easycube-time.txt ../VerifyData/synth-easycube-time.txt 1 2 5 1 0 > ../VerifyData/easycube3125.tmp &
+./hypertraps-all.ce ../VerifyData/synth-cross-samples-0.txt ../VerifyData/synth-cross-times-0.txt ../VerifyData/synth-cross-times-0.txt 1 2 5 0 0 > ../VerifyData/cross03125.tmp &
+./hypertraps-all.ce ../VerifyData/synth-cross-samples-1.txt ../VerifyData/synth-cross-times-1.txt ../VerifyData/synth-cross-times-1.txt 1 2 5 0 0 > ../VerifyData/cross13125.tmp &
+./hypertraps-all.ce ../VerifyData/synth-cross-samples-2.txt ../VerifyData/synth-cross-times-2.txt ../VerifyData/synth-cross-times-2.txt 1 2 5 0 0 > ../VerifyData/cross23125.tmp &
+./hypertraps-all.ce ../VerifyData/synth-hardcube-data.txt ../VerifyData/synth-hardcube-time.txt ../VerifyData/synth-hardcube-time.txt 1 2 5 0 0 > ../VerifyData/hardcube3125.tmp &
+./hypertraps-all.ce ../VerifyData/synth-easycube-data.txt ../VerifyData/synth-easycube-time.txt ../VerifyData/synth-easycube-time.txt 1 2 5 0 0 > ../VerifyData/easycube3125.tmp &
