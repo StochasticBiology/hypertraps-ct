aae5b6e1eb2d9b9b0ebbf571b9c05b1c28b24a6f
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2021-01-25 11:46:14 +0100

    Add files via upload

--- Process/prepare-all.sh
+++ Process/prepare-all.sh
@@ -1,5 +1,12 @@
+# shift raw data to folder for manipulation
+mkdir ../Data/
+cp ../RawData/* ../Data
+
 chmod +x *.sh
 
+# prepare TB data
 ./cook-data.sh ../Data/ng.2878-S2.txt ../Data/tuberculosis-v5-header-19-29.csv 1000
+# prepare MRO-NCBI data
 ./cook-data.sh ../Data/mro-ncbi-tree.phy ../Data/mro-barcodes.csv 0.1
+# prepare MRO-TT data (special steps needed)
 ./mro-timetree-parse.sh
