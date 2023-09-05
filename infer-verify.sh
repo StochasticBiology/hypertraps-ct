52ed7c5b9607fe3f441b9538fdcb328acd1b9caa
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-09-05 14:46:02 +0200

    Add files via upload

--- infer-verify.sh
+++ infer-verify.sh
@@ -1,9 +1,11 @@
 # generate synthetic datasets
 mkdir VerifyData/
 cd Verify
+gcc -o3 generate.c -lm -o generate.ce
 gcc -o3 generate-easycube.c -lm -o generate-easycube.ce
 gcc -o3 generate-hardcube.c -lm -o generate-hardcube.ce
 gcc -o3 generate-cross.c -lm -o generate-cross.ce
+./generate.ce 78 > generated.tmp
 ./generate-easycube.ce
 ./generate-hardcube.ce
 ./generate-cross.ce
@@ -13,6 +15,17 @@ cd ../Inference
 
 # compile and run hypertraps code
 gcc -o3 hypertraps-all.c -lm -o hypertraps-all.ce
+
+./hypertraps-all.ce ../VerifyData/synth-0-data.txt ../VerifyData/synth-0-times.txt ../VerifyData/synth-0-times.txt 1 3 3 0 0 > ../VerifyData/tmp-synth-1 &
+./hypertraps-all.ce ../VerifyData/synth-0-data.txt ../VerifyData/synth-0-times.txt ../VerifyData/synth-0-times.txt 2 3 3 0 0 > ../VerifyData/tmp-synth-2 &
+
+./hypertraps-all.ce ../VerifyData/synth-1-data.txt ../VerifyData/synth-1-times.txt ../VerifyData/synth-1-times.txt 1 3 3 0 0 > ../VerifyData/tmp-synth-3 &
+./hypertraps-all.ce ../VerifyData/synth-1-data.txt ../VerifyData/synth-1-times.txt ../VerifyData/synth-1-times.txt 2 3 3 0 0 > ../VerifyData/tmp-synth-4 &
+
+./hypertraps-all.ce ../VerifyData/synth-2-data.txt ../VerifyData/synth-2-times.txt ../VerifyData/synth-2-times.txt 1 3 3 0 0 > ../VerifyData/tmp-synth-5 &
+./hypertraps-all.ce ../VerifyData/synth-2-data.txt ../VerifyData/synth-2-times.txt ../VerifyData/synth-2-times.txt 2 3 3 0 0 > ../VerifyData/tmp-synth-6 &
+
+
 ./hypertraps-all.ce ../VerifyData/synth-cross-samples-0.txt ../VerifyData/synth-cross-times-0.txt ../VerifyData/synth-cross-times-0.txt 1 2 5 0 0 > ../VerifyData/cross03125.tmp &
 ./hypertraps-all.ce ../VerifyData/synth-cross-samples-1.txt ../VerifyData/synth-cross-times-1.txt ../VerifyData/synth-cross-times-1.txt 1 2 5 0 0 > ../VerifyData/cross13125.tmp &
 ./hypertraps-all.ce ../VerifyData/synth-cross-samples-2.txt ../VerifyData/synth-cross-times-2.txt ../VerifyData/synth-cross-times-2.txt 1 2 5 0 0 > ../VerifyData/cross23125.tmp &
