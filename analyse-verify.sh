8de43032da50821348d50b6c44bbc1aa3ca2c10e
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-09-05 14:50:05 +0200

    Add files via upload

--- analyse-verify.sh
+++ analyse-verify.sh
@@ -3,6 +3,13 @@ cd Inference
 
 # compile and run posterior analysis code
 gcc -o3 posteriors.c -lm -o posteriors.ce
+./posteriors.ce 0 10 ../VerifyData/synth-0-data.txt-posterior-1-1-3-3-0.txt > ../VerifyData/posterior-synth-0-1.tmp &
+./posteriors.ce 0 10 ../VerifyData/synth-0-data.txt-posterior-1-2-3-3-0.txt > ../VerifyData/posterior-synth-0-2.tmp &
+./posteriors.ce 0 10 ../VerifyData/synth-1-data.txt-posterior-1-1-3-3-0.txt > ../VerifyData/posterior-synth-1-1.tmp &
+./posteriors.ce 0 10 ../VerifyData/synth-1-data.txt-posterior-1-2-3-3-0.txt > ../VerifyData/posterior-synth-1-2.tmp &
+./posteriors.ce 0 10 ../VerifyData/synth-2-data.txt-posterior-1-1-3-3-0.txt > ../VerifyData/posterior-synth-2-1.tmp &
+./posteriors.ce 0 10 ../VerifyData/synth-2-data.txt-posterior-1-2-3-3-0.txt > ../VerifyData/posterior-synth-2-2.tmp &
+
 ./posteriors.ce 0 10 ../VerifyData/synth-easycube-data.txt-posterior-1-1-2-5-0.txt > ../VerifyData/posterior-easycube.tmp &
 ./posteriors.ce 0 10 ../VerifyData/synth-hardcube-data.txt-posterior-1-1-2-5-0.txt > ../VerifyData/posterior-hardcube.tmp &
 ./posteriors.ce 0 10 ../VerifyData/synth-cross-samples-0.txt-posterior-1-1-2-5-0.txt > ../VerifyData/posterior-cross-0.tmp &
