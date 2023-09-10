8db591b6042d261fe3464658f4affc346dd3e9a4
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-09-10 20:47:34 +0200

    Use labels to simplify TB and MRO output

--- infer-mro.sh
+++ infer-mro.sh
@@ -2,21 +2,21 @@ cd Inference
 gcc -o3 hypertraps-all.c -lm -o hypertraps-all.ce
 
 ### NCBI, discrete time
-./hypertraps-all.ce --obs ../Data/mro-ncbi-tree.phy-cooked.txt-data.txt --seed 1 --length 5 --kernel 7 --losses > ../Data/mro-expt-1.txt &
-./hypertraps-all.ce --obs ../Data/mro-ncbi-tree.phy-cooked.txt-data.txt --seed 2 --length 5 --kernel 7 --losses > ../Data/mro-expt-2.txt &
+./hypertraps-all.ce --obs ../Data/mro-ncbi-tree.phy-cooked.txt-data.txt --seed 1 --length 5 --kernel 7 --losses --label ../Data/mro-1 > ../Data/mro-expt-1.txt &
+./hypertraps-all.ce --obs ../Data/mro-ncbi-tree.phy-cooked.txt-data.txt --seed 2 --length 5 --kernel 7 --losses --label ../Data/mro-2 > ../Data/mro-expt-2.txt &
 
 ### TimeTree, discrete time
-./hypertraps-all.ce --obs ../Data/mro-tree-tt-format.phy-data.txt --seed 1 --length 5 --kernel 7 --losses > ../Data/mro-expt-3.txt &
-./hypertraps-all.ce --obs ../Data/mro-tree-tt-format.phy-data.txt --seed 2 --length 5 --kernel 7 --losses > ../Data/mro-expt-4.txt &
+./hypertraps-all.ce --obs ../Data/mro-tree-tt-format.phy-data.txt --seed 1 --length 5 --kernel 7 --losses --label ../Data/mro-3 > ../Data/mro-expt-3.txt &
+./hypertraps-all.ce --obs ../Data/mro-tree-tt-format.phy-data.txt --seed 2 --length 5 --kernel 7 --losses --label ../Data/mro-4 > ../Data/mro-expt-4.txt &
 
 ### TimeTree, continuous time
-./hypertraps-all.ce --obs ../Data/mro-tree-tt-format.phy-data.txt --times ../Data/mro-tree-tt-format.phy-datatime.txt --seed 1 --length 6 --kernel 7 --losses > ../Data/mro-expt-5.txt &
-./hypertraps-all.ce --obs ../Data/mro-tree-tt-format.phy-data.txt --times ../Data/mro-tree-tt-format.phy-datatime.txt --seed 2 --length 6 --kernel 7 --losses > ../Data/mro-expt-6.txt &
+./hypertraps-all.ce --obs ../Data/mro-tree-tt-format.phy-data.txt --times ../Data/mro-tree-tt-format.phy-datatime.txt --seed 1 --length 6 --kernel 7 --losses --label ../Data/mro-5 > ../Data/mro-expt-5.txt &
+./hypertraps-all.ce --obs ../Data/mro-tree-tt-format.phy-data.txt --times ../Data/mro-tree-tt-format.phy-datatime.txt --seed 2 --length 6 --kernel 7 --losses --label ../Data/mro-6 > ../Data/mro-expt-6.txt &
 
 ### TimeTree+, discrete time
-./hypertraps-all.ce --obs ../Data/mro-tree-ttplus-format.phy-data.txt --seed 1 --length 5 --kernel 7 --losses > ../Data/mro-expt-7.txt &
-./hypertraps-all.ce --obs ../Data/mro-tree-ttplus-format.phy-data.txt --seed 2 --length 5 --kernel 7 --losses > ../Data/mro-expt-8.txt &
+./hypertraps-all.ce --obs ../Data/mro-tree-ttplus-format.phy-data.txt --seed 1 --length 5 --kernel 7 --losses --label ../Data/mro-7 > ../Data/mro-expt-7.txt &
+./hypertraps-all.ce --obs ../Data/mro-tree-ttplus-format.phy-data.txt --seed 2 --length 5 --kernel 7 --losses --label ../Data/mro-8 > ../Data/mro-expt-8.txt &
 
 ### TimeTree+, continuous time
-./hypertraps-all.ce --obs ../Data/mro-tree-ttplus-format.phy-data.txt --times ../Data/mro-ttplus-1.txt --endtimes ../Data/mro-ttplus-2.txt --seed 1 --length 6 --kernel 7 --losses > ../Data/mro-expt-9.txt &
-./hypertraps-all.ce --obs ../Data/mro-tree-ttplus-format.phy-data.txt --times ../Data/mro-ttplus-1.txt --endtimes ../Data/mro-ttplus-2.txt --seed 2 --length 6 --kernel 7 --losses > ../Data/mro-expt-10.txt &
+./hypertraps-all.ce --obs ../Data/mro-tree-ttplus-format.phy-data.txt --times ../Data/mro-ttplus-1.txt --endtimes ../Data/mro-ttplus-2.txt --seed 1 --length 6 --kernel 7 --losses --label ../Data/mro-9 > ../Data/mro-expt-9.txt &
+./hypertraps-all.ce --obs ../Data/mro-tree-ttplus-format.phy-data.txt --times ../Data/mro-ttplus-1.txt --endtimes ../Data/mro-ttplus-2.txt --seed 2 --length 6 --kernel 7 --losses --label ../Data/mro-10 > ../Data/mro-expt-10.txt &
--- infer-tb.sh
+++ infer-tb.sh
@@ -2,13 +2,13 @@ cd Inference
 gcc -o3 hypertraps-all.c -lm -o hypertraps-all.ce
 
 # discrete time approaches
-./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --seed 1 --length 5 --kernel 4 > ../Data/tb-dt-1.tmp &
-./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --seed 2 --length 5 --kernel 4 > ../Data/tb-dt-2.tmp &
+./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --seed 1 --length 5 --kernel 4 --label ../Data/tb-dt-1 > ../Data/tb-dt-1.tmp &
+./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --seed 2 --length 5 --kernel 4 --label ../Data/tb-dt-2 > ../Data/tb-dt-2.tmp &
 
 # continuous time approaches
-./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 1 --length 6 --kernel 4 > ../Data/tb-ct-1.tmp &
-./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 2 --length 6 --kernel 4 > ../Data/tb-ct-2.tmp &
-./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 1 --length 7 --kernel 4 > ../Data/tb-ct-a-1.tmp &
-./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 2 --length 7 --kernel 4 > ../Data/tb-ct-a-2.tmp &
-./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 1 --length 6 --kernel 5 > ../Data/tb-ct-b-1.tmp &
-./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 2 --length 6 --kernel 5 > ../Data/tb-ct-b-2.tmp &
+./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 1 --length 6 --kernel 4 --label ../Data/tb-ct-1 > ../Data/tb-ct-1.tmp &
+./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 2 --length 6 --kernel 4 --label ../Data/tb-ct-2 > ../Data/tb-ct-2.tmp &
+./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 1 --length 7 --kernel 4 --label ../Data/tb-ct-a-1 > ../Data/tb-ct-a-1.tmp &
+./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 2 --length 7 --kernel 4 --label ../Data/tb-ct-a-2 > ../Data/tb-ct-a-2.tmp &
+./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 1 --length 6 --kernel 5 --label ../Data/tb-ct-b-1 > ../Data/tb-ct-b-1.tmp &
+./hypertraps-all.ce --obs ../Data/ng.2878-S2.txt-cooked.txt-data.txt --times ../Data/ng.2878-S2.txt-cooked.txt-datatime.txt --seed 2 --length 6 --kernel 5 --label ../Data/tb-ct-b-2 > ../Data/tb-ct-b-2.tmp &
