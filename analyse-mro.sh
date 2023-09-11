ab306f51c1cde90031368f7cea15077210e3b0ad
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-09-11 06:57:19 +0200

    Posterior analysis for science examples

---
+++ analyse-mro.sh
@@ -0,0 +1,16 @@
+cd Inference
+gcc -o3 posteriors.c -lm 0o posteriors.ce
+
+# direct time
+./posteriors.ce --posterior ../Data/mro-1-posterior.txt > ../Data/mro-1-a.tmp &
+./posteriors.ce --posterior ../Data/mro-2-posterior.txt > ../Data/mro-2-a.tmp &
+
+# continuous time
+./posteriors.ce --posterior ../Data/mro-3-posterior.txt > ../Data/mro-3-a.tmp &
+./posteriors.ce --posterior ../Data/mro-4-posterior.txt > ../Data/mro-4-a.tmp &
+./posteriors.ce --posterior ../Data/mro-5-posterior.txt > ../Data/mro-5-a.tmp &
+./posteriors.ce --posterior ../Data/mro-6-posterior.txt > ../Data/mro-6-a.tmp &
+./posteriors.ce --posterior ../Data/mro-7-posterior.txt > ../Data/mro-7-a.tmp &
+./posteriors.ce --posterior ../Data/mro-8-posterior.txt > ../Data/mro-8-a.tmp &
+./posteriors.ce --posterior ../Data/mro-9-posterior.txt > ../Data/mro-9-a.tmp &
+./posteriors.ce --posterior ../Data/mro-10-posterior.txt > ../Data/mro-10-a.tmp &
---
+++ analyse-tb.sh
@@ -0,0 +1,14 @@
+cd Inference
+gcc -o3 posteriors.c -lm 0o posteriors.ce
+
+# direct time
+./posteriors.ce --posterior ../Data/tb-dt-1-posterior.txt > ../Data/tb-dt-1-a.tmp &
+./posteriors.ce --posterior ../Data/tb-dt-2-posterior.txt > ../Data/tb-dt-2-a.tmp &
+
+# continuous time
+./posteriors.ce --posterior ../Data/tb-ct-1-posterior.txt > ../Data/tb-ct-1-a.tmp &
+./posteriors.ce --posterior ../Data/tb-ct-2-posterior.txt > ../Data/tb-ct-2-a.tmp &
+./posteriors.ce --posterior ../Data/tb-ct-a-1-posterior.txt > ../Data/tb-ct-a-1-a.tmp &
+./posteriors.ce --posterior ../Data/tb-ct-a-2-posterior.txt > ../Data/tb-ct-a-2-a.tmp &
+./posteriors.ce --posterior ../Data/tb-ct-b-1-posterior.txt > ../Data/tb-ct-b-1-a.tmp &
+./posteriors.ce --posterior ../Data/tb-ct-b-2-posterior.txt > ../Data/tb-ct-b-2-a.tmp &
