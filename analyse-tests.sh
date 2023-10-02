7f8951868f0990fe9ddaa9456cd393908c6916e8
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-10-02 10:35:22 +0200

    Test beds for general parameter structures

--- analyse-tests.sh
+++ analyse-tests.sh
@@ -2,9 +2,36 @@ cd Inference
 
 gcc -o3 posteriors.c -lm -o posteriors.ce
 
+## discrete time
+# different sampling walkers
 ./posteriors.ce --posterior ../VerifyData/test-cross-1-posterior.txt > ../VerifyData/t-simple-1-a.tmp &
 ./posteriors.ce --posterior  ../VerifyData/test-cross-2-posterior.txt  > ../VerifyData/t-simple-2-a.tmp &
 ./posteriors.ce --posterior  ../VerifyData/test-cross-3-posterior.txt  > ../VerifyData/t-simple-3-a.tmp &
+# different parameter structures
+./posteriors.ce --posterior ../VerifyData/test-cross-mod--1-posterior.txt --model -1 > ../VerifyData/t-sample-mod--1-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-0-posterior.txt --model 0 > ../VerifyData/t-sample-mod-0-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-1-posterior.txt --model 1 > ../VerifyData/t-sample-mod-1-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-2-posterior.txt --model 2 > ../VerifyData/t-sample-mod-2-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-3-posterior.txt --model 3 > ../VerifyData/t-sample-mod-3-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-4-posterior.txt --model 4 > ../VerifyData/t-sample-mod-4-a.tmp &
+
+### discrete time XOR problem
+./posteriors.ce --posterior ../VerifyData/test-xor-mod--1-posterior.txt --model -1 > ../VerifyData/t-xor-mod--1-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-xor-mod-0-posterior.txt --model 0 > ../VerifyData/t-xor-mod-0-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-xor-mod-1-posterior.txt --model 1 > ../VerifyData/t-xor-mod-1-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-xor-mod-2-posterior.txt --model 2 > ../VerifyData/t-xor-mod-2-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-xor-mod-3-posterior.txt --model 3 > ../VerifyData/t-xor-mod-3-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-xor-mod-4-posterior.txt --model 4 > ../VerifyData/t-xor-mod-4-a.tmp &
+
+## continuous time
+# different sampling walkers
 ./posteriors.ce --posterior  ../VerifyData/test-cross-ct-1-posterior.txt > ../VerifyData/t-simple-ct-1-a.tmp &
 ./posteriors.ce --posterior  ../VerifyData/test-cross-ct-2-posterior.txt > ../VerifyData/t-simple-ct-2-a.tmp &
 ./posteriors.ce --posterior  ../VerifyData/test-cross-ct-3-posterior.txt > ../VerifyData/t-simple-ct-3-a.tmp &
+# different parameter structures
+./posteriors.ce --posterior ../VerifyData/test-cross-ct-mod--1-posterior.txt --model -1 > ../VerifyData/t-sample-ct-mod--1-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-ct-mod-0-posterior.txt --model 0 > ../VerifyData/t-sample-ct-mod-0-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-ct-mod-1-posterior.txt --model 1 > ../VerifyData/t-sample-ct-mod-1-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-ct-mod-2-posterior.txt --model 2 > ../VerifyData/t-sample-ct-mod-2-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-ct-mod-3-posterior.txt --model 3 > ../VerifyData/t-sample-ct-mod-3-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-ct-mod-4-posterior.txt --model 4 > ../VerifyData/t-sample-ct-mod-4-a.tmp &
--- infer-tests.sh
+++ infer-tests.sh
@@ -4,6 +4,9 @@ mkdir VerifyData/
 cd Verify
 gcc -o3 generate-cross.c -lm -o generate-cross.ce
 ./generate-cross.ce
+gcc -o3 generate-xor.c -lm -o generate-xor.ce
+./generate-xor.ce
+
 cp synth* ../VerifyData
 
 cd ../Inference
@@ -18,6 +21,22 @@ gcc -o3 hypertraps-all.c -lm -o hypertraps-all.ce
 # different optimisers
 ./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --length 4 --label ../VerifyData/test-cross-sa --sa > ../VerifyData/t-simple-sa.tmp &
 ./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --length 4 --label ../VerifyData/test-cross-sgd --sgd > ../VerifyData/t-simple-sgd.tmp &
+# different parameter structures
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --length 4 --label ../VerifyData/test-cross-mod--1 --model -1 > ../VerifyData/t-simple-mod--1.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --length 4 --label ../VerifyData/test-cross-mod-0 --model 0 > ../VerifyData/t-simple-mod-0.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --length 4 --label ../VerifyData/test-cross-mod-1 --model 1 > ../VerifyData/t-simple-mod-1.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --length 4 --label ../VerifyData/test-cross-mod-2 --model 2 > ../VerifyData/t-simple-mod-2.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --length 4 --label ../VerifyData/test-cross-mod-3 --model 3 > ../VerifyData/t-simple-mod-3.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --length 4 --label ../VerifyData/test-cross-mod-4 --model 4 > ../VerifyData/t-simple-mod-4.tmp &
+
+### discrete time XOR problem
+./hypertraps-all.ce --obs ../VerifyData/synth-xor.txt --length 4 --label ../VerifyData/test-xor-mod--1 --model -1 > ../VerifyData/t-xor-mod--1.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-xor.txt --length 4 --label ../VerifyData/test-xor-mod-0 --model 0 > ../VerifyData/t-xor-mod-0.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-xor.txt --length 4 --label ../VerifyData/test-xor-mod-1 --model 1 > ../VerifyData/t-xor-mod-1.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-xor.txt --length 4 --label ../VerifyData/test-xor-mod-2 --model 2 > ../VerifyData/t-xor-mod-2.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-xor.txt --length 4 --label ../VerifyData/test-xor-mod-3 --model 3 > ../VerifyData/t-xor-mod-3.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-xor.txt --length 4 --label ../VerifyData/test-xor-mod-4 --model 4 > ../VerifyData/t-xor-mod-4.tmp &
+
 
 ## continuous time
 # different sampling walkers
@@ -27,3 +46,11 @@ gcc -o3 hypertraps-all.c -lm -o hypertraps-all.ce
 # different optimisers
 ./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --times ../VerifyData/synth-cross-times-0.txt --length 4 --label ../VerifyData/test-cross-ct-sa --sa > ../VerifyData/t-simple-ct-sa.tmp &
 ./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --times ../VerifyData/synth-cross-times-0.txt --length 4 --label ../VerifyData/test-cross-ct-sgd --sgd > ../VerifyData/t-simple-ct-sgd.tmp &
+# different parameter structures
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --times ../VerifyData/synth-cross-times-0.txt --length 4 --label ../VerifyData/test-cross-ct-mod--1 --model -1 > ../VerifyData/t-simple-ct-mod--1.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --times ../VerifyData/synth-cross-times-0.txt --length 4 --label ../VerifyData/test-cross-ct-mod-0 --model 0 > ../VerifyData/t-simple-ct-mod-0.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --times ../VerifyData/synth-cross-times-0.txt --length 4 --label ../VerifyData/test-cross-ct-mod-1 --model 1 > ../VerifyData/t-simple-ct-mod-1.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --times ../VerifyData/synth-cross-times-0.txt --length 4 --label ../VerifyData/test-cross-ct-mod-2 --model 2 > ../VerifyData/t-simple-ct-mod-2.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --times ../VerifyData/synth-cross-times-0.txt --length 4 --label ../VerifyData/test-cross-ct-mod-3 --model 3 > ../VerifyData/t-simple-ct-mod-3.tmp &
+./hypertraps-all.ce --obs ../VerifyData/synth-cross-samples-0.txt --times ../VerifyData/synth-cross-times-0.txt --length 4 --label ../VerifyData/test-cross-ct-mod-4 --model 4 > ../VerifyData/t-simple-ct-mod-4.tmp &
+
