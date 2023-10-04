fbe93fb9349017bfaa0e8d20d411860a84b740e5
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-10-04 06:51:06 +0200

    Test beds and plot code for general param structure

--- analyse-tests.sh
+++ analyse-tests.sh
@@ -8,20 +8,24 @@ gcc -o3 posteriors.c -lm -o posteriors.ce
 ./posteriors.ce --posterior  ../VerifyData/test-cross-2-posterior.txt  > ../VerifyData/t-simple-2-a.tmp &
 ./posteriors.ce --posterior  ../VerifyData/test-cross-3-posterior.txt  > ../VerifyData/t-simple-3-a.tmp &
 # different parameter structures
-./posteriors.ce --posterior ../VerifyData/test-cross-mod--1-posterior.txt --model -1 > ../VerifyData/t-sample-mod--1-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-cross-mod-0-posterior.txt --model 0 > ../VerifyData/t-sample-mod-0-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-cross-mod-1-posterior.txt --model 1 > ../VerifyData/t-sample-mod-1-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-cross-mod-2-posterior.txt --model 2 > ../VerifyData/t-sample-mod-2-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-cross-mod-3-posterior.txt --model 3 > ../VerifyData/t-sample-mod-3-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-cross-mod-4-posterior.txt --model 4 > ../VerifyData/t-sample-mod-4-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod--1-posterior.txt --model -1 --verbose > ../VerifyData/t-sample-mod--1-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-0-posterior.txt --model 0 --verbose > ../VerifyData/t-sample-mod-0-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-1-posterior.txt --model 1 --verbose > ../VerifyData/t-sample-mod-1-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-2-posterior.txt --model 2 --verbose > ../VerifyData/t-sample-mod-2-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-3-posterior.txt --model 3 --verbose > ../VerifyData/t-sample-mod-3-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-cross-mod-4-posterior.txt --model 4 --verbose > ../VerifyData/t-sample-mod-4-a.tmp &
 
-### discrete time XOR problem
-./posteriors.ce --posterior ../VerifyData/test-xor-mod--1-posterior.txt --model -1 > ../VerifyData/t-xor-mod--1-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-xor-mod-0-posterior.txt --model 0 > ../VerifyData/t-xor-mod-0-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-xor-mod-1-posterior.txt --model 1 > ../VerifyData/t-xor-mod-1-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-xor-mod-2-posterior.txt --model 2 > ../VerifyData/t-xor-mod-2-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-xor-mod-3-posterior.txt --model 3 > ../VerifyData/t-xor-mod-3-a.tmp &
-./posteriors.ce --posterior ../VerifyData/test-xor-mod-4-posterior.txt --model 4 > ../VerifyData/t-xor-mod-4-a.tmp &
+### discrete time hi-order logic problem
+./posteriors.ce --posterior ../VerifyData/test-ho-mod--1-posterior.txt --model -1 --verbose > ../VerifyData/t-ho-mod--1-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-ho-mod-2-posterior.txt --model 2 --verbose > ../VerifyData/t-ho-mod-2-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-ho-mod-3-posterior.txt --model 3 --verbose > ../VerifyData/t-ho-mod-3-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-ho-mod--1-l-posterior.txt --model -1 --verbose > ../VerifyData/t-ho-mod--1-l-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-ho-mod-2-l-posterior.txt --model 2 --verbose > ../VerifyData/t-ho-mod-2-l-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-ho-mod-3-l-posterior.txt --model 3 --verbose > ../VerifyData/t-ho-mod-3-l-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-ho-mod--1-sa-best.txt --model -1 --sims 1000 --verbose > ../VerifyData/t-ho-mod--1-sa-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-ho-mod-2-sa-best.txt --model 2 --sims 1000 --verbose > ../VerifyData/t-ho-mod-2-sa-a.tmp &
+./posteriors.ce --posterior ../VerifyData/test-ho-mod-3-sa-best.txt --model 3 --sims 1000 --verbose > ../VerifyData/t-ho-mod-3-sa-a.tmp &
+
 
 ## continuous time
 # different sampling walkers
