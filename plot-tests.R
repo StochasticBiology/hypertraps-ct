5d1c12a68e9f4c8f39c9070f7db3125cc2b12c04
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-10-23 13:23:20 +0200

    Consolidation updates

--- plot-tests.R
+++ plot-tests.R
@@ -2,18 +2,18 @@ library(ggplot2)
 library(gridExtra)
 
 # compare bubble plot summaries with walker numbers and DT/CT
-df.1 = read.csv("VerifyData/test-cross-1-posterior.txt-bubbles.csv") 
+df.1 = read.csv("VerifyData/test-cross-1-bubbles.csv") 
 df.1$expt = 1; df.1$t = 1; df.1$label = "DT n=200"
-df.2 = read.csv("VerifyData/test-cross-2-posterior.txt-bubbles.csv") 
+df.2 = read.csv("VerifyData/test-cross-2-bubbles.csv") 
 df.2$expt = 2; df.2$t = 1; df.2$label = "DT n=2000"
-df.3 = read.csv("VerifyData/test-cross-3-posterior.txt-bubbles.csv") 
+df.3 = read.csv("VerifyData/test-cross-3-bubbles.csv") 
 df.3$expt = 3; df.3$t = 1; df.3$label = "DT n=20"
 
-df.4 = read.csv("VerifyData/test-cross-ct-1-posterior.txt-bubbles.csv") 
+df.4 = read.csv("VerifyData/test-cross-ct-1-bubbles.csv") 
 df.4$expt = 1; df.4$t = 2; df.4$label = "CT n=200"
-df.5 = read.csv("VerifyData/test-cross-ct-2-posterior.txt-bubbles.csv") 
+df.5 = read.csv("VerifyData/test-cross-ct-2-bubbles.csv") 
 df.5$expt = 2; df.5$t = 2; df.5$label = "CT n=2000"
-df.6 = read.csv("VerifyData/test-cross-ct-3-posterior.txt-bubbles.csv") 
+df.6 = read.csv("VerifyData/test-cross-ct-3-bubbles.csv") 
 df.6$expt = 3; df.6$t = 2; df.6$label = "CT n=20"
 df = rbind(df.1, df.2, df.3, df.4, df.5, df.6)
 
@@ -21,15 +21,15 @@ g.bubbles = ggplot(df, aes(x=Time+expt/10,y=OriginalIndex+t/10,size=Probability,
   geom_point() + xlab("Time") + ylab("Feature") + theme_light()
 
 # compare likelihood values by walker number for direct time experiments
-lik.1 = read.csv("VerifyData/test-cross-1-lik.txt")
+lik.1 = read.csv("VerifyData/test-cross-1-lik.csv")
 lik.1$expt = "MCMC n=200"
-lik.2 = read.csv("VerifyData/test-cross-2-lik.txt")
+lik.2 = read.csv("VerifyData/test-cross-2-lik.csv")
 lik.2$expt = "MCMC n=2000"
-lik.3 = read.csv("VerifyData/test-cross-3-lik.txt")
+lik.3 = read.csv("VerifyData/test-cross-3-lik.csv")
 lik.3$expt = "MCMC n=20"
-lik.4 = read.csv("VerifyData/test-cross-sa-lik.txt")
+lik.4 = read.csv("VerifyData/test-cross-sa-lik.csv")
 lik.4$expt = "SA"
-lik.5 = read.csv("VerifyData/test-cross-sgd-lik.txt")
+lik.5 = read.csv("VerifyData/test-cross-sgd-lik.csv")
 lik.5$expt = "SGD"
 
 lik.df = rbind(lik.1, lik.2, lik.3)
@@ -44,15 +44,15 @@ g.dt.lik = ggplot(lik.ts.df, aes(x=Step, y=LogLikelihood1, color=expt)) +
   geom_line() + ylab("log L") + theme_light()
 
 # compare likelihood values by walker number for continuous time experiments
-lik.1 = read.csv("VerifyData/test-cross-ct-1-lik.txt")
+lik.1 = read.csv("VerifyData/test-cross-ct-1-lik.csv")
 lik.1$expt = "MCMC n=200"
-lik.2 = read.csv("VerifyData/test-cross-ct-2-lik.txt")
+lik.2 = read.csv("VerifyData/test-cross-ct-2-lik.csv")
 lik.2$expt = "MCMC n=2000"
-lik.3 = read.csv("VerifyData/test-cross-ct-3-lik.txt")
+lik.3 = read.csv("VerifyData/test-cross-ct-3-lik.csv")
 lik.3$expt = "MCMC n=20"
-lik.4 = read.csv("VerifyData/test-cross-ct-sa-lik.txt")
+lik.4 = read.csv("VerifyData/test-cross-ct-sa-lik.csv")
 lik.4$expt = "SA"
-lik.5 = read.csv("VerifyData/test-cross-ct-sgd-lik.txt")
+lik.5 = read.csv("VerifyData/test-cross-ct-sgd-lik.csv")
 lik.5$expt = "SGD"
 
 lik.df = rbind(lik.1, lik.2, lik.3)
