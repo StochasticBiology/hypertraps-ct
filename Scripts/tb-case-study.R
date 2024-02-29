a0f7cd6b351d9c63be501a6bbe0686ca36059262
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-02-29 10:54:33 +0100

    TB case study with new R curation

--- Scripts/tb-case-study.R
+++ Scripts/tb-case-study.R
@@ -4,19 +4,18 @@ setwd("Scripts")
 
 require(parallel)
 
-# read prepared data on transitions for TB case study
-tb.tab = as.matrix(read.table("../Data/ng.2878-S2.txt-cooked.txt-data.txt"))
-tb.srcs = tb.tab[seq(1, nrow(tb.tab), by = 2),]
-tb.dests = tb.tab[seq(2, nrow(tb.tab), by = 2),]
-
-tb.times = as.matrix(read.table("../Data/ng.2878-S2.txt-cooked.txt-datatime.txt"))*1000
+# read and curate data on transitions for TB case study
+tb.set = curate.tree("../Data/ng.2878-S2.txt", "../Data/tuberculosis-v5-header-19-29.csv")
+tb.srcs = tb.set$srcs
+tb.dests = tb.set$dests
+tb.times = tb.set$times*1000
 
 # for parallelisation -- with different control parameter "fork" this function will return different HyperTraPS experiments
 parallel.fn = function(fork, srcs, dests, times) {
   if(fork == 1) { return(HyperTraPS(dests, initialstates = srcs, samplegap = 10, seed = 1, length = 4, kernel = 3))}
   if(fork == 2) { return(HyperTraPS(dests, initialstates = srcs, samplegap = 10, seed = 2, length = 4, kernel = 3))}
-  if(fork == 3) { return(HyperTraPS(dests, initialstates = srcs, samplegap = 10, penalty = 1, seed = 1, length = 4, kernel = 3))}
-  if(fork == 4) { return(HyperTraPS(dests, initialstates = srcs, samplegap = 10, penalty = 1, seed = 2, length = 4, kernel = 3))}
+  if(fork == 3) { return(HyperTraPS(dests, initialstates = srcs, samplegap = 10, starttimes = times, endtimes = times, penalty = 1, seed = 1, length = 4, kernel = 3))}
+  if(fork == 4) { return(HyperTraPS(dests, initialstates = srcs, samplegap = 10, starttimes = times, endtimes = times, penalty = 1, seed = 2, length = 4, kernel = 3))}
   if(fork == 5) { return(HyperTraPS(dests, initialstates = srcs, samplegap = 10, endtimes = times, seed = 1, length = 4, kernel = 3))}
   if(fork == 6) { return(HyperTraPS(dests, initialstates = srcs, samplegap = 10, endtimes = times, seed = 2, length = 4, kernel = 3))}
   if(fork == 7) { return(HyperTraPS(dests, initialstates = srcs, samplegap = 10, endtimes = times, penalty = 1, seed = 1, length = 4, kernel = 3))}
@@ -27,9 +26,9 @@ parallel.fn = function(fork, srcs, dests, times) {
 
 # run these experiments in parallel
 n.fork = 10
-expt.names = c("DT, seed 1", "DT, seed 2", "DT, penalty, seed 1", "DT, penalty, seed 2", 
-               "CT, seed 1", "CT, seed 2", "CT, penalty, seed 1", "CT, penalty, seed 2",
-               "CT pen mod 3 seed 1", "CT pen mod 3 seed 2")
+expt.names = c("DT, seed 1", "DT, seed 2", "CT precise, penalty, seed 1", "CT precise, penalty, seed 2", 
+               "CT wide, seed 1", "CT wide, seed 2", "CT wide, penalty, seed 1", "CT wide, penalty, seed 2",
+               "CT wide, pen mod 3 seed 1", "CT wide, pen mod 3 seed 2")
 
 parallelised.runs2 <- mcmapply(parallel.fn, fork=1:n.fork,
                                MoreArgs = list(src = tb.srcs,
@@ -76,7 +75,7 @@ ggarrange(plotlist = lapply(parallelised.runs2[9:10], plotHypercube.influencegra
 
 # various plot collections
 ggarrange(plotlist = lapply(parallelised.runs2, plotHypercube.influencegraph, thresh = 0, cv.thresh = 1.5))
-ggarrange(plotlist = lapply(parallelised.runs2, plotHypercube.influences, cv.thresh = 1.5, featurenames = tb.names))
+#ggarrange(plotlist = lapply(parallelised.runs2, plotHypercube.influences, cv.thresh = 1.5, featurenames = tb.names))
 
 # produce summary plots for each of the experiments
 cv.ts = c(0.3, 0.3, 1.5, 1.5, 0.3, 0.3, 1.5, 1.5, 1.5, 1.5)
