2825d902fc6254598d9b765cef456da32d119619
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-03-07 09:34:52 +0100

    🧪 Updated TB case study including predictions

--- Scripts/tb-case-study.R
+++ Scripts/tb-case-study.R
@@ -17,20 +17,22 @@ parallel.fn = function(fork, srcs, dests, times) {
   if(fork == 2) { return(HyperTraPS(dests, initialstates = srcs, seed = 2, length = 5, kernel = 3))}
   if(fork == 3) { return(HyperTraPS(dests, initialstates = srcs, starttimes = times, endtimes = times, penalty = 1, seed = 1, length = 5, kernel = 3))}
   if(fork == 4) { return(HyperTraPS(dests, initialstates = srcs, starttimes = times, endtimes = times, penalty = 1, seed = 2, length = 5, kernel = 3))}
-  if(fork == 5) { return(HyperTraPS(dests, initialstates = srcs, endtimes = times, seed = 1, length = 5, kernel = 3))}
-  if(fork == 6) { return(HyperTraPS(dests, initialstates = srcs, endtimes = times, seed = 2, length = 5, kernel = 3))}
+  if(fork == 5) { return(HyperTraPS(dests, initialstates = srcs, starttimes=times*0.75, endtimes = times*1.25, penalty = 1, seed = 1, length = 5, kernel = 3))}
+  if(fork == 6) { return(HyperTraPS(dests, initialstates = srcs, starttimes=times*0.75, endtimes = times*1.25, penalty = 1, seed = 2, length = 5, kernel = 3))}
   if(fork == 7) { return(HyperTraPS(dests, initialstates = srcs, endtimes = times, penalty = 1, seed = 1, length = 5, kernel = 3))}
   if(fork == 8) { return(HyperTraPS(dests, initialstates = srcs, endtimes = times, penalty = 1, seed = 2, length = 5, kernel = 3))}
-  if(fork == 9) { return(HyperTraPS(dests, initialstates = srcs, endtimes = times, penalty = 1, seed = 1, model = 3, length = 5, kernel = 3))}
-  if(fork == 10) { return(HyperTraPS(dests, initialstates = srcs, endtimes = times, penalty = 1, seed = 2, model = 3, length = 5, kernel = 3))}
+  if(fork == 9) { return(HyperTraPS(dests, initialstates = srcs, starttimes=times, endtimes = times, penalty = 1, seed = 1, model = 3, length = 5, kernel = 3))}
+  if(fork == 10) { return(HyperTraPS(dests, initialstates = srcs, starttimes=times, endtimes = times, penalty = 1, seed = 2, model = 3, length = 5, kernel = 3))}
+  if(fork == 11) { return(HyperTraPS(dests, initialstates = srcs, starttimes=times, endtimes = times, penalty = 3, seed = 1, model = 3, length = 5, kernel = 3))}
+  if(fork == 12) { return(HyperTraPS(dests, initialstates = srcs, starttimes=times, endtimes = times, penalty = 3, seed = 2, model = 3, length = 5, kernel = 3))}
 }
 
 # run these experiments in parallel
-n.fork = 10
-expt.names = c("DT, seed 1", "DT, seed 2", "CT precise, penalty, seed 1", "CT precise, penalty, seed 2", 
-               "CT wide, seed 1", "CT wide, seed 2", "CT wide, penalty, seed 1", "CT wide, penalty, seed 2",
-               "CT wide, pen mod 3 seed 1", "CT wide, pen mod 3 seed 2")
+n.fork = 12
 
+# labels for experiments and features
+expt.names = c("DT 1", "DT 2", "CT 1", "CT 2", "CTu 1", "CTu 2", "CTw 1", "CTw 2", "CTm3 1", "CT m3 2", "CT m3p 1", "CT m3p 2")
+tb.names = colnames(tb.set$data[2:ncol(tb.set$data)])
 parallelised.runs2 <- mcmapply(parallel.fn, fork=1:n.fork,
                                MoreArgs = list(src = tb.srcs,
                                                dests = tb.dests,
@@ -38,9 +40,13 @@ parallelised.runs2 <- mcmapply(parallel.fn, fork=1:n.fork,
                                SIMPLIFY = FALSE,
                                mc.cores = min(detectCores(), n.fork))
 
-# labels for experiments and features
 
-tb.names = colnames(tb.set$data[2:ncol(tb.set$data)])
+####### some checks for consistency across different random seeds
+ggarrange(plotHypercube.influencegraph(parallelised.runs2[[3]], featurenames=tb.names, cv.thresh = cv.thresh),
+          plotHypercube.influencegraph(parallelised.runs2[[4]], featurenames=tb.names, cv.thresh = cv.thresh) )
+
+ggarrange(plotHypercube.influencegraph(parallelised.runs2[[11]], featurenames=tb.names, cv.thresh = cv.thresh),
+          plotHypercube.influencegraph(parallelised.runs2[[12]], featurenames=tb.names, cv.thresh = cv.thresh) )
 
 # pull the "bubble" plot data together for comparison
 bdf = thdf = data.frame()
@@ -56,7 +62,7 @@ for(i in 1:n.fork) {
 bdf$ExptLabel = expt.names[bdf$Expt]
 
 # bubble plot comparing experiments
-g.tb.bubbles = ggplot(bdf, aes(x=Time+Expt/10, y=OriginalIndex, size=Probability, color=ExptLabel)) +
+g.tb.bubbles = ggplot(bdf, aes(x=Time+Expt/20, y=OriginalIndex, size=Probability, color=ExptLabel)) +
   geom_point(alpha=0.5) + labs(x = "Ordering", y = "Feature", color = "Experiment") +
   theme_light()
 
@@ -67,6 +73,58 @@ dev.off()
 
 # look at likelihood traces
 ggarrange(plotlist = lapply(parallelised.runs2, plotHypercube.lik.trace))
+
+##########
+
+# main text figure: penalised likelihood, precisely specified timings
+png("plot-tb-main-text.png", width=800*sf, height=600*sf, res=72*sf)
+ggarrange(
+  g.curated.tree,
+  plotHypercube.sampledgraph2(parallelised.runs2[[3]], use.arc = FALSE, featurenames = tb.names, 
+                              edge.label.size=3, edge.label.angle = "none", node.labels=FALSE,
+                              no.times=TRUE, small.times=TRUE) + theme(legend.position="none"),
+  plotHypercube.influencegraph(parallelised.runs2[[3]], cv.thresh=0.3, featurenames=tb.names),
+  plotHypercube.motifseries(parallelised.runs2[[4]], t.set=c(1, 5, 10, 15, 20, 50), label.size=2, thresh = 0.02),
+  widths=c(1,2,1,2),
+  labels=c("A", "B", "C", "D")
+  
+)
+dev.off()
+
+# SI figure: alternative plots, discrete time, uncertain time specification, tripletwise model
+
+png("plot-tb-si.png", width=800*sf, height=1000*sf, res=72*sf)
+ggarrange( 
+  ggarrange(plotHypercube.influences(parallelised.runs2[[3]], featurenames=tb.names, reorder=TRUE,
+                                     cv.thresh = 0.3),
+            plotHypercube.motifs(parallelised.runs2[[3]], featurenames=tb.names) + theme(legend.position="none"),
+            plotHypercube.timeseries(parallelised.runs2[[3]], featurenames=tb.names), 
+            nrow=1,
+            labels=c("A", "B", "C")),
+  
+  ggarrange(
+    plotHypercube.sampledgraph2(parallelised.runs2[[1]], use.arc = FALSE, featurenames = tb.names, 
+                                edge.label.size=3, edge.label.angle = "none", node.labels=FALSE,
+                                no.times=TRUE, small.times=FALSE) + theme(legend.position="none"),
+    plotHypercube.influencegraph(parallelised.runs2[[7]], cv.thresh=0.3, featurenames=tb.names), 
+    widths=c(2,1),
+    nrow=1,
+    labels=c("D", "E")),
+  
+  ggarrange(
+    plotHypercube.sampledgraph2(parallelised.runs2[[7]], use.arc = FALSE, featurenames = tb.names, 
+                                edge.label.size=3, edge.label.angle = "none", node.labels=FALSE,
+                                no.times=TRUE, small.times=TRUE) + theme(legend.position="none"),
+    plotHypercube.influencegraph(parallelised.runs2[[11]], cv.thresh=0.3, featurenames=tb.names),
+    widths=c(2,1),
+    nrow=1,
+    labels=c("F", "G")),
+  nrow=3
+)
+dev.off()
+
+
+# other plots for curiosity
 
 # influence graphs for all the L^2 cases
 ggarrange(plotlist = lapply(parallelised.runs2[1:8], plotHypercube.influencegraph, thresh = 0, cv.thresh = 0.3))
@@ -79,36 +137,45 @@ ggarrange(plotlist = lapply(parallelised.runs2, plotHypercube.influencegraph, th
 #ggarrange(plotlist = lapply(parallelised.runs2, plotHypercube.influences, cv.thresh = 1.5, featurenames = tb.names))
 
 # produce summary plots for each of the experiments
-cv.ts = c(0.3, 0.3, 1.5, 1.5, 0.3, 0.3, 1.5, 1.5, 1.5, 1.5)
+cv.ts = rep(0.4, n.fork)
 for(expt in 1:n.fork) {
   # focus on one penalised likelihood, continuous time case
   tb.post.1 = parallelised.runs2[[expt]]
   
   g.tb.graph = plotHypercube.sampledgraph2(tb.post.1, use.arc = FALSE, featurenames = tb.names, 
-                                            edge.label.size=3, edge.label.angle = "none", node.labels=FALSE,
-                                            no.times=TRUE, small.times=TRUE)
+                                           edge.label.size=3, edge.label.angle = "none", node.labels=FALSE,
+                                           no.times=TRUE, small.times=TRUE)
   g.tb.thist = plotHypercube.timehists(tb.post.1, featurenames = tb.names)
   g.tb.motifs = plotHypercube.motifs(tb.post.1, featurenames=tb.names)
-  g.tb.influences = plotHypercube.influences(tb.post.1, featurenames=tb.names, reorder=TRUE,
-                                             cv.thresh = cv.ts[expt])
+  
   g.tb.tseries = plotHypercube.timeseries(tb.post.1, featurenames=tb.names)
   
   g.tb.influencegraph = plotHypercube.influencegraph(tb.post.1, featurenames=tb.names,
                                                      cv.thresh = cv.ts[expt])
-  g.tb.motifseries = plotHypercube.motifseries(tb.post.1, t.set=c(0.001, 0.01, 0.1, 0.5, 1, 5, 10))
+  if(expt <= 4 | expt >= 11) {
+    this.t.set=c(0.001, 0.01, 0.1, 0.5, 1, 5, 10)
+  } else {
+    this.t.set=c(1e-5,1e-4,2e-4,5e-4,1e-3,1e-2)
+  }
+  g.tb.motifseries = plotHypercube.motifseries(tb.post.1, t.set=this.t.set)
   
   png(paste0("plot-tb-summary-", expt, ".png"), width=800*sf, height=800*sf, res=72*sf)
   print(ggarrange(ggarrange(g.curated.tree, g.tb.graph + theme(legend.position="none"), widths=c(1,2), nrow=1, labels=c("A", "B")), 
                   ggarrange(g.tb.influencegraph, g.tb.motifseries, widths=c(1,2), nrow=1, labels=c("C", "D")), nrow=2))
   dev.off()
   
-  png(paste0("plot-tb-summary-si-", expt, ".png"), width=1000*sf, height=400*sf, res=72*sf)
-  print(ggarrange(g.tb.influences, g.tb.motifs, g.tb.tseries, nrow=1, labels=c("A", "B", "C")))
-  dev.off()
+  if(tb.post.1$model == 2) {
+    g.tb.influences = plotHypercube.influences(tb.post.1, featurenames=tb.names, reorder=TRUE,
+                                               cv.thresh = cv.ts[expt])
+    png(paste0("plot-tb-summary-si-", expt, ".png"), width=1000*sf, height=400*sf, res=72*sf)
+    print(ggarrange(g.tb.influences, g.tb.motifs, g.tb.tseries, nrow=1, labels=c("A", "B", "C")))
+    dev.off()
+  }
 }
 
 summary.list = lapply(parallelised.runs2, plotHypercube.lik.trace)
 ggarrange(plotlist=summary.list)
+
 
 
 
---
+++ Scripts/tb-predict-precise.R
@@ -0,0 +1,118 @@
+setwd("..")
+source("hypertraps.R")
+setwd("Scripts")
+
+require(parallel)
+
+# read and curate data on transitions for TB case study
+tb.set = curate.tree("../Data/ng.2878-S2.txt", "../Data/tuberculosis-v5-header-19-29.csv")
+tb.srcs = tb.set$srcs
+tb.dests = tb.set$dests
+tb.times = tb.set$times*1000
+tb.names = colnames(tb.set$data[2:ncol(tb.set$data)])
+
+nsplits = 5
+L = ncol(tb.set$srcs)
+
+training.refs = list()
+nobs = nrow(tb.set$srcs)
+set.seed(1)
+for(i in 1:nsplits) {
+  training.refs[[i]] = sample(1:nobs, 0.75*nobs)
+}
+
+# for parallelisation -- with different control parameter "fork" this function will return different HyperTraPS experiments
+parallel.fn = function(fork, srcs, dests, times, training.refs) {
+  train.srcs = srcs[training.refs[[fork]],]
+  train.dests = dests[training.refs[[fork]],]
+  train.times = times[training.refs[[fork]]]
+  return(HyperTraPS(train.dests, initialstates = train.srcs, starttimes = train.times, endtimes = train.times, penalty = 1, seed = 1, length = 5, kernel = 3))
+}
+
+# run these experiments in parallel
+parallelised.runs <- mcmapply(parallel.fn, fork=1:nsplits,
+                               MoreArgs = list(src = tb.srcs,
+                                               dests = tb.dests,
+                                               times = tb.times,
+                                               training.refs = training.refs),
+                               SIMPLIFY = FALSE,
+                               mc.cores = min(detectCores(), nsplits))
+
+# labels for experiments and features
+res.df = res.state.df = data.frame()
+for(i in 1:nsplits) {
+  test.refs = setdiff(1:nobs, training.refs[[i]])
+  for(j in 1:length(test.refs)) {
+    state = tb.dests[test.refs[j],]
+    
+    prev.state = tb.srcs[test.refs[j],]
+    if(sum(state) - sum(prev.state) == 1) {
+      prediction = predictNextStep(parallelised.runs[[i]], prev.state)
+      true.dest = paste0(state, collapse="")
+      predicted.ref = which(prediction$states == true.dest)
+      predicted.ranking = nrow(prediction)+1-rank(prediction$probs)[predicted.ref]
+      predicted.prob = prediction$probs[predicted.ref]
+      predicted.mode = prediction$states[prediction$probs==max(prediction$probs)]
+      tdf = data.frame(splitref = i,
+                       true.dest = true.dest, 
+                       predicted.mode = predicted.mode,
+                       predicted.ranking=predicted.ranking, 
+                       predicted.prob=predicted.prob)
+      res.state.df = rbind(res.state.df, tdf)
+    }
+    tdf = as.data.frame(t(state))
+    hidden.locus = sample(1:L, 1)
+    true.val = state[hidden.locus]
+    state[hidden.locus] = 2
+    prediction = predictHiddenVals(parallelised.runs[[i]], state) 
+    prob.1 = prediction$locus.probs$prob
+    tdf$splitref = i
+    tdf$sample = j
+    tdf$hidden.locus = hidden.locus
+    tdf$true.val = true.val
+    tdf$prob.1 = prob.1
+    tdf$hit = ifelse( (prob.1 > 0.5 & true.val == 1) | (prob.1 < 0.5 & true.val == 0), TRUE, FALSE)
+    res.df = rbind(res.df, tdf)
+    
+    
+  }
+}
+#ggplot(res.state.df, aes(x=predicted.ranking, fill=factor(splitref))) + 
+g.future = ggplot(res.state.df[res.state.df$splitref==1,], aes(x=factor(predicted.ranking))) + #, fill=factor(splitref))) + 
+  geom_histogram(stat="count") +
+  labs(x="Prediction ranking of next step", y="Number of test observations", fill="Sample") +
+  theme_minimal()
+
+this.split = 3
+test.refs = setdiff(1:nobs, training.refs[[this.split]])
+x = as.data.frame(tb.dests[test.refs,])
+colnames(x) = tb.names
+x$sample = 1:nrow(x)
+xm = melt(x, id.vars = c("sample"))
+preds = data.frame(variable=tb.names[res.df$hidden.locus], splitref=res.df$splitref, sample=res.df$sample, hit=res.df$hit)
+preds = preds[preds$splitref==this.split,]
+g.hidden = ggplot() + geom_tile(data=xm, aes(x=variable, y=sample, fill=factor(value))) + 
+  scale_fill_manual(values = c("white", "#CCCCCC")) +
+  geom_point(data=preds, aes(x=variable, y=sample, color=hit)) + theme_minimal() +
+  labs(x="Drug", y="Test observation", color="Prediction correct", fill="True values")
+
+roc.df = data.frame()
+threshes = c((0:200)/400, 0.4999, 0.49999)
+for(thresh in threshes) {
+  unclass = length(which( (res.df$prob.1 < 0.5+thresh) &
+                          (res.df$prob.1 > 0.5-thresh) ))
+  trues = length(which( (res.df$true.val==1 & res.df$prob.1 > 0.5+thresh) |
+                            (res.df$true.val==0 & res.df$prob.1 < 0.5-thresh) ))
+  falses = (nrow(res.df)-unclass) - trues
+  roc.df = rbind(roc.df, data.frame(thresh=thresh,
+                                    unclass=unclass/nrow(res.df),
+                                    trues=trues/(trues+falses)))
+}
+g.roc = ggplot(roc.df, aes(x=unclass, y=trues)) + geom_point() +
+  theme_light() +
+  labs(x="Proportion of observations\nwithout predictions", y="Proportion of correct predictions")
+
+sf = 2
+png("plot-tb-predictions-precise.png", width=800*sf, height=300*sf, res=72*sf)
+ggarrange(g.future, g.hidden, g.roc, nrow=1, labels=c("A", "B", "C"), widths=c(1,2,1))
+dev.off()
